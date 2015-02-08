b50c2eccb4a90bb260875fda4107203a8e5ce188
unknown <Bryan.Overseth@att.com>
2015-02-07 17:41:21 -0700

    Added json content to resumeBuilder.js

---
+++ frontend-nanodegree-resume/js/resumeBuilder.js
@@ -0,0 +1,59 @@
+// var name = "Bryan R. Overseth";
+// var formattedName = HTMLheaderName.replace("%data%", name);
+
+// var role = "Sr. Systems Architect";
+// var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+// $("#header").prepend(formattedName);
+// $("#header").append(formattedRole);
+
+var bio = {
+	"name" : "Bryan R. Overseth",
+	"role" : "Sr. Systems Architect",
+	"contacts" : {
+		"mobile" : "678-386-6979",
+		"email" : "Boverseth@hotmail.com",
+		"github" : "https://github.com/BryanOverseth",
+		"twitter" : "NA",
+		"location" : "Bigfork, MT"
+	},
+	"welcomeMessage" : "Welcome to my site.  Please let me know if you have any questions",
+	"skills" : ["Architect", "Design and Engineering", "Consulting", "Technical Training", "Project Management", "Concept Labs", "Technical Writing"],
+	"bioPic": "images/Overssth.jpg"
+}
+
+var work = {}
+	work.position =  "Sr. Systems Architect";
+	work.employer =  "AT&T";
+	work.years =  "21";
+
+var eduction = {
+	"schools": [
+		{
+			"name": "Dekalb",
+			"url": "http://www.gpc.edu/",
+			"city":  "Atlanta Area",
+			"degree": "",
+			"years": "1+",
+			"minor": ["Business Administration"]
+		},
+		{
+			"name": "Devry",
+			"url": "http://www.devry.edu/",
+			"city":  "Atlanta Area",
+			"degree": "",
+			"years": "2+",
+			"major": ["Telecommunications Management", "Computer Science"]
+		},
+		{
+			"name": "Udacity",
+			"url": "http://www.udacity.com/",
+			"city":  "Online",
+			"degree": "NanoDegree",
+			"years": "8 months",
+			"major": ["Front-End Web Developer"]
+		}
+	]
+}
+
+
