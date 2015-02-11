d9d82c577913c80f08f71866c2f5c58ce186f57d
unknown <Bryan.Overseth@att.com>
2015-02-11 12:55:48 -0700

    Update resumeBuilder JSON

---
+++ js/TestCode.js
@@ -0,0 +1,70 @@
+// var name = "Bryan R. Overseth";
+// var formattedName = HTMLheaderName.replace("%data%", name);
+
+// var role = "Sr. Systems Architect";
+// var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+// $("#header").prepend(formattedName);
+// $("#header").append(formattedRole);
+
+// $("#main").append("Bryan R. Overseth")
+
+// [string].replace([old],[new])
+
+// var email = "Bryan.Overseth@att.com";
+
+// var newemail =
+//	email.replace("att", "hotmail");
+
+// console.log(email);
+// console.log(newemail);
+
+// var awesomethoughts = "My name is Bryan R. Overseth and I am AWESOME!"
+
+// var funthoughts =
+//	awesomethoughts.replace("AWESOME", "FUN");
+
+// $("#main").append(funthoughts);
+
+// console.log(awesomethoughts);
+
+// ----------------------------------------------------
+
+// var s = "audacity";
+
+// var udacitiyizer = function(s) {
+//	s = s[1].toUpperCase() + s.slice(2);
+//	return s;
+//	};
+
+// console.log(udacitiyizer(s));
+
+// ----------------------------------------------------
+
+// function incrementLastArrayElement(_array) {
+//	var newArray = [];
+//	newArray = _array.slice(0);
+//	var lastNumber = newArray.pop();
+//	newArray.push(lastNumber + 1);
+//	return newArray;
+//}
+
+//console.log(incrementLastArrayElement(newArray));
+
+//------------------------------------------------------
+
+// var name = "AlbERt EINstEiN";
+
+// function nameChanger(oldName) {
+//	var finalName = oldName;
+//	var names = oldName.split(" ");
+//	names[1] = names[1].toUpperCase();
+//	names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
+//	finalName = names.join(" ");
+//	return finalName;
+//}
+
+
+//console.log(nameChanger(name));
+
+// ------------------------------------------------------
---
+++ js/resumeBuilder.js
@@ -0,0 +1,91 @@
+var bio = {
+	"name" : "Bryan R. Overseth",
+	"role" : "Sr. Systems Architect",
+	"contacts" : {
+		"mobile" : "678-386-6979",
+		"email" : "Boverseth@hotmail.com",
+		"github" : "https://github.com/BryanOverseth",
+		"twitter" : "NA",
+		"location" : "Bigfork, MT"
+	},
+	"welcomeMessage" : "Welcome to my site.  Please let me know if you have any questions",
+	"skills" : ["Architect", "Design and Engineering", "Consulting", "Technical Training", "Project Management", "Concept Labs", "Technical Writing"],
+	"bioPic": "images/Overssth.jpg"
+}
+
+var work = {}
+	work.position =  "Sr. Systems Architect";
+	work.employer =  "AT&T";
+	work.years =  "21";
+
+var eduction = {
+	"schools": [
+		{
+			"name": "Dekalb",
+			"url": "http://www.gpc.edu/",
+			"city":  "Atlanta Area",
+			"degree": "NA",
+			"years": "1+",
+			"minor": ["Business Administration"]
+		},
+		{
+			"name": "Devry",
+			"url": "http://www.devry.edu/",
+			"city":  "Atlanta Area",
+			"degree": "NA",
+			"years": "2+",
+			"major": ["Telecommunications Management", "Computer Science"]
+		}
+	]
+},
+	"onlineCourses": [
+		{
+			"name": "Udacity",
+			"url": "http://www.udacity.com/",
+			"city":  "Online",
+			"degree": "NanoDegree",
+			"years": "8 months",
+			"major": ["Front-End Web Developer"]
+		}
+	]
+}
+
+var work = {
+	"jobs": [
+		{
+			"employer": "AT&T",
+			"title": "Senior Technical Architect",
+			"location":  "Atlanta, GA",
+			"dates": "2007-Present",
+			"description": "Enterprise Technology Services"
+		},
+		{
+			"employer": "Cingular Wireless",
+			"title": "Architect and Engineering",
+			"location":  "Atlanta, GA",
+			"dates": "2001-2007",
+			"description": "Infrastructure Architecture and Mobility Services"
+		},
+		{
+			"employer": "Cypress Communications",
+			"title": "Principal IT Engineer",
+			"location":  "Atlanta, GA",
+			"dates": "2000-2001",
+			"description": "Information Technology Services"
+		},
+		{
+			"employer": "Bellsouth Technical Services Inc.",
+			"title": "Systems Analyst",
+			"location":  "Atlanta, GA",
+			"dates": "1999-2000",
+			"description": "Corporate Datacenter Services"
+		},
+		{
+			"employer": "Bellsouth Mobility",
+			"title": "Senior Network Engineer",
+			"location":  "Atlanta, GA",
+			"dates": "1998-1999",
+			"description": "Regional Datacenter Services"
+		}
+	]
+}
