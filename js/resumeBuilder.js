2d09ceb4ba21b623d85207763de10d8378be2d2a
unknown <Bryan.Overseth@att.com>
2015-03-06 23:14:48 -0700

    Added work.displaywork(); to resumeBuilder.js Added function inName(name) to resumeBuilder.js Added function inName(name) to resumeBuilder.js Added $('#main').append(internationalizeButton); to resumeBuilder.js Added $(document).click(function(loc) to resumeBuilder.js Added $("#mapDiv").append(googleMap); to resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -52,5 +52,83 @@ var bio = {
 // biography function - builds name and role and contact information
 bio.biography();
 
+var work = {
+	"jobs": [
+		{
+			"employer": "AT&T",
+			"title": "Senior Technical Architect",
+			"location":  "Bigfork, MT",
+			"dates": "2007-Present",
+			"description": "Enterprise Technology Services"
+		},
+		{
+			"employer": "Cingular Wireless",
+			"title": "Architect and Engineering",
+			"location":  "Alpharetta, GA",
+			"dates": "2001-2007",
+			"description": "Infrastructure Architecture and Mobility Services"
+		},
+		{
+			"employer": "Cypress Communications",
+			"title": "Principal IT Engineer",
+			"location":  "Buckhead, GA",
+			"dates": "2000-2001",
+			"description": "Information Technology Services"
+		},
+		{
+			"employer": "Bellsouth Technical Services Inc.",
+			"title": "Systems Analyst",
+			"location":  "Atlanta, GA",
+			"dates": "1999-2000",
+			"description": "Corporate Datacenter Services"
+		},
+		{
+			"employer": "Bellsouth Mobility",
+			"title": "Senior Network Engineer",
+			"location":  "Atlanta, GA",
+			"dates": "1998-1999",
+			"description": "Regional Datacenter Services"
+		}
+	],
+	"displaywork" : function () {
+			for (job in work.jobs) {
+				$("#workExperience").append(HTMLworkStart);
+				var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+				var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+				var formattedEmployerTitle = formattedEmployer.concat(formattedTitle);
+				$(".work-entry:last").append(formattedEmployerTitle);
+				var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+				$(".work-entry:last").append(formattedDates);
+				var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+				$(".work-entry:last").append(formattedDescription);
+		}
+	}
+};
+// work function - builds work related information
+work.displaywork();
 
 
+// function that changes the case of bio.name
+function inName(name) {
+	name = bio.name.trim().split(" ");
+	console.log(name);
+	name[2] = name[2].toUpperCase();
+	name[0] = name[0].slice(0,1).toUpperCase() +
+		name[0].slice(1).toLowerCase();
+
+	return name[0] +" "+ name[1] +" "+ name[2];
+}
+
+// Internationlize bio.name
+$('#main').append(internationalizeButton);
+
+// Console x abd y coordinates of a mouse click on the web page
+$(document).click(function(loc) {
+	var x = loc.pageX;
+	var y = loc.pageY;
+
+	logClicks(x,y);
+});
+
+//display Google map locations
+$("#mapDiv").append(googleMap);
